9bee597722efec114ebbc63579f2d9606705f9a8
LapitskayaN <lapitskaya.nl@gmail.com>
2021-03-02 16:52:26 +0300

    workshop react router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from "./components/navbar"
+import Home from "./components/home"
+import MovieList from "./components/movie-list"
+import ClickedMovie from "./components/clicked-movie"
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>hello Wilders!</p>
+        <Navbar />
+        <Switch> 
+          <Route exact path="/" render={(props)=> <Home {...props}/>} />
+          <Route exact path="/list" render={(props)=> <MovieList {...props}/>} />
+          <Route exact path="/movie" render={(props)=> <ClickedMovie {...props}/>} />
+        </Switch>
       </header>
     </div>
   );
